4c7ed79022ad3046bb455b8a0234da5ff20015bd
baktash07 <35742714+baktash07@users.noreply.github.com>
2018-09-07 23:04:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,26 +1,15 @@
 
 const Discord = require("discord.js");
-const Client = new Discord.Client();
-const prefix = '!';
-Client.on('message' , message => {
-  if(message.content.startsWith(prefix + voicesetup)) {
-  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('x you dont have the permeissions');
-  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('i dont have permissions');
-  var args = message.content.split(' ').slice(1).join(' ');
-  if(args && !args.includes(0)) return message.channel.send('__you must type 0 in the room name __');
-  if(!args) args = `VoiceOnline [ ${message.guild.members.filter(s = s.voiceChannel).size} ]`;
-  message.channel.send('» Done');
-  message.guild.createChannel(`${args.replace(0, message.guild.members.filter(s = s.voiceChannel).size)}`, 'voice').then(c = {
-    c.overwritePermissions(message.guild.id, {
-      CONNECT false,
-      SPEAK false
-    });
-    setInterval(() = {
-      c.setName(`${args.replace(0, message.guild.members.filter(s = s.voiceChannel).size)}`).catch(err = {
-        if(err) return;
-      });
-    },3000);
-  });
-  }
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+
+client.on('voiceStateUpdate', (old, now) => {
+  const channel = client.channels.get('487304344149164052');
+  const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
+  const size = channel.name.match(/\[\s(\d+)\s\]/);
+  if (!size) return channel.setName(`Voice Online: ${currentSize}`);
+  if (currentSize !== size) channel.setName(`Voice Online: ${currentSize}`);
 });
-client.login(process.env.BOT_TOKEN);
+
+client.login("NDY4ODE0MzAzOTg2NzEyNTg5.DnRvbA.vZk96mAw4Rh6aZMIIJkmrymSE6I");

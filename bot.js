b7e78059c0103133ce2e21266938a5e80e27fd47
bakutkisa241 <35742714+bakutkisa241@users.noreply.github.com>
2018-09-12 19:28:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('voiceStateUpdate', (old, now) => {
   const channel = client.channels.get('489465001229287437');
   const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
   const size = channel.name.match(/\[\s(\d+)\s\]/);
-  if (!size) return channel.setName(`Voice Online: ${currentSize}`);
+  if (!size) return channel.setName(`Voice Online ${currentSize}`);
   if (currentSize !== size) channel.setName(`Voice Online: ${currentSize}`);
 });
 
